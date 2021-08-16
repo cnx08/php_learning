615d45ee82be3c75f8f977c8c0c3a41f2290f1f3
Bondar <bondar.a.n@effective-group.ru>
2021-08-16 18:58:44 +0300

    Настройка уровня логрования, установка bootstrap

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -4,19 +4,21 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * Class LuckyController
  * @package App\Controller
  */
-class LuckyController
+class LuckyController extends AbstractController
 {
     public function number(): Response
     {
+
         $number = random_int(0, 100);
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+        return $this->render('lucky/number.html.twig', [
+            'number' => $number,
+        ]);
     }
 }
